d923b04b89ff82376e03457d1d1448c212b14a43
Martin Marinov <m.marinov1733@gmail.com>
2025-09-21 23:00:40 +0300

    видео

---
+++ Models/Video.cs
@@ -0,0 +1,12 @@
+﻿namespace Sapphire17.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public byte[]? ImageData { get; set; }
+        public string? ImageMimeType { get; set; }
+    }
+}
