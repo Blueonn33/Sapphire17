9a542ba604bbedcea0a8496d11e28756034eec2f
Martin Marinov <m.marinov1733@gmail.com>
2025-09-21 22:58:50 +0300

    бележки

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,6 +1,21 @@
-﻿namespace Sapphire17.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sapphire17.Models
 {
     public class Note
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[]? ImageData { get; set; }
+        public string? ImageMimeType { get; set; }
+        public bool Important { get; set; }
+        public string Theme { get; set; }
+        public bool Completed { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
     }
 }
