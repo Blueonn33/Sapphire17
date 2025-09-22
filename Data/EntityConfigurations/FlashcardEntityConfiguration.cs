5160df245c439f9c5bd922769a6aba571175c53b
Martin Marinov <m.marinov1733@gmail.com>
2025-09-22 23:08:00 +0300

    flashcard entity configuration

---
+++ Data/EntityConfigurations/FlashcardEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sapphire17.Models;
+
+namespace Sapphire17.Data.EntityConfigurations
+{
+    public class FlashcardEntityConfiguration : IEntityTypeConfiguration<Flashcard>
+    {
+        public void Configure(EntityTypeBuilder<Flashcard> builder)
+        {
+            builder.ToTable("Flashcards");
+
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.Question).IsRequired().HasMaxLength(200);
+            builder.Property(f => f.Answer).IsRequired();
+
+            builder.HasOne(f => f.Deck)
+                .WithMany(d => d.Flashcards)
+                .HasForeignKey(f => f.DeckId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
