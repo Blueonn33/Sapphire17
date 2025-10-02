a5fe56efb2e1d4fb23e773d3d519565c832ac0dc
Martin Marinov <m.marinov1733@gmail.com>
2025-10-02 16:06:13 +0300

    IDeckRepository

---
+++ Repositories/DeckRepository.cs
@@ -0,0 +1,17 @@
+﻿using Sapphire17.Data;
+using Sapphire17.Repositories.Interfaces;
+
+namespace Sapphire17.Repositories
+{
+    public class DeckRepository : IDeckRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ISetRepository _setRepository;
+
+        public DeckRepository(ApplicationDbContext context, ISetRepository setRepository)
+        {
+            _context = context;
+            _setRepository = setRepository;
+        }
+    }
+}
---
+++ Repositories/Interfaces/IDeckRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sapphire17.Models;
+
+namespace Sapphire17.Repositories.Interfaces
+{
+    public interface IDeckRepository
+    {
+        public Task<IEnumerable<Deck>> GetAllDecksBySetIdAsync(int setId);
+        public Task<Deck?> GetDeckByIdAsync(int deckId);
+        public Task CreateDeckAsync(Deck deck);
+        public Task UpdateDeckAsync(Deck deck);
+        public Task DeleteDeckAsync(int deckId);
+    }
+}
