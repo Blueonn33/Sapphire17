bedbf0010d11bca1c6e59460ec1f7b7ae9183b6c
Martin Marinov <m.marinov1733@gmail.com>
2025-10-08 23:05:07 +0300

    note repository

---
+++ Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sapphire17.Models;
+
+namespace Sapphire17.Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+        public Task<IEnumerable<Note>> GetAllNotesByUserIdAsync(string userId);
+        public Task<Note?> GetNoteByIdAsync(int id);
+        public Task CreateNoteAsync(Note note);
+        public Task UpdateNoteAsync(Note note);
+        public Task DeleteNoteAsync(int id);
+    }
+}
