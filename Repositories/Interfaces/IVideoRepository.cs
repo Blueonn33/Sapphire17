189e16bc0f47fa900e90e69a5e5ce33d84ec961f
Martin Marinov <m.marinov1733@gmail.com>
2025-10-16 14:28:53 +0300

    video repository

---
+++ Repositories/Interfaces/IVideoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sapphire17.Models;
+
+namespace Sapphire17.Repositories.Interfaces
+{
+    public interface IVideoRepository
+    {
+        public Task<IEnumerable<Video>> GetAllVideosByUserIdAsync(string userId);
+        public Task<Video?> GetVideoByIdAsync(int id);
+        public Task CreateVideoAsync(Video video);
+        public Task UpdateVideoAsync(Video video);
+        public Task DeleteVideoAsync(int id);
+    }
+}
