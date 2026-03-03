c991590183c7f3112a6504f38e6ecec247387d0b
Martin Marinov <m.marinov1733@gmail.com>
2026-03-03 15:55:27 +0200

    миграция

---
+++ Migrations/20260303135505_Reactions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sapphire17.Migrations
+{
+    /// <inheritdoc />
+    public partial class Reactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoReactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Reaction = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    VideoId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoReactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoReactions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_VideoReactions_Videos_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoReactions_UserId",
+                table: "VideoReactions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoReactions_VideoId",
+                table: "VideoReactions",
+                column: "VideoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoReactions");
+        }
+    }
+}
