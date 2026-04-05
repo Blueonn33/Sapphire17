8041b493c70a85c046f9aba9af2009c46e5a6e5b
Martin Marinov <m.marinov1733@gmail.com>
2026-04-05 14:51:51 +0300

    IQuizCollectionRepo

--- Repositories/Interfaces/IQuizCollectionRepository.cs
+++ Repositories/Interfaces/IQuizCollectionRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Sapphire17.Repositories.Interfaces
+﻿using Sapphire17.Models;
+
+namespace Sapphire17.Repositories.Interfaces
 {
     public interface IQuizCollectionRepository
     {
-    }
+        public Task<IEnumerable<QueryCollection>> GetAllQuizCollections();
+        public Task<QuizCollectionRepository?> GetQuizCollectionById(int quizCollectionId);
+        public Task CreateQuizCollection(QuizCollection quizCollection);
+        public Task UpdateQuizCollection(QuizCollection quizCollection);
+        public Task DeleteQuizCollection(int quizCollectionId);
+    };
 }
--- Repositories/QuizCollectionRepository.cs
+++ Repositories/QuizCollectionRepository.cs
@@ -1,8 +1,33 @@
-﻿using Sapphire17.Repositories.Interfaces;
+﻿using Sapphire17.Models;
+using Sapphire17.Repositories.Interfaces;
 
 namespace Sapphire17.Repositories
 {
     public class QuizCollectionRepository : IQuizCollectionRepository
     {
+        public Task CreateQuizCollection(QuizCollection quizCollection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteQuizCollection(int quizCollectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<QueryCollection>> GetAllQuizCollections()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QuizCollectionRepository?> GetQuizCollectionById(int quizCollectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateQuizCollection(QuizCollection quizCollection)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
