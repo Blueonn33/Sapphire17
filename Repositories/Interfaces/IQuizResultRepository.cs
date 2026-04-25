4cedb3f3d7557a8c367ed7d2dc1547ba936431a9
Martin Marinov <m.marinov1733@gmail.com>
2026-04-25 22:17:40 +0300

    quiz result repository

---
+++ Repositories/Interfaces/IQuizResultRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sapphire17.Models;
+
+namespace Sapphire17.Repositories.Interfaces
+{
+    public interface IQuizResultRepository
+    {
+        public Task<IEnumerable<QuizResult>> GetQuizResultByQuizCollectionAndUser(int quizCollectionId, string userId);
+        public Task<QuizResult?> GetQuizResultById(int quizResultId);
+        public Task CreateQuizResult(QuizResult quizResult);
+    }
+}
---
+++ Repositories/QuizResultRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sapphire17.Data;
+using Sapphire17.Models;
+using Sapphire17.Repositories.Interfaces;
+
+namespace Sapphire17.Repositories
+{
+    public class QuizResultRepository : IQuizResultRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuizResultRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<QuizResult>> GetQuizResultByQuizCollectionAndUser(int quizCollectionId, string userId)
+        {
+            var quizResult = await _context.QuizResults
+                .Where(q => q.QuizCollectionId == quizCollectionId && q.UserId == userId)
+                .OrderByDescending(q => q.DateCompleted)
+                .ToListAsync();
+
+            return quizResult;
+        }
+
+        public async Task<QuizResult?> GetQuizResultById(int quizResultId)
+        {
+            var quizResult = await _context.QuizResults.FindAsync(quizResultId);
+            return quizResult;
+        }
+
+        public async Task CreateQuizResult(QuizResult quizResult)
+        {
+            await _context.QuizResults.AddAsync(quizResult);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
